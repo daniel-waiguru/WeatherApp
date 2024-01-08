181a47c91cba1124bb321edc51d92d127508e86b
Daniel Waiguru <danielirungu91@gmail.com>
2024-01-08 16:30:53 +0300

    add copyright

--- data/src/test/java/com/danielwaiguru/weatherapp/data/repositories/WeatherRepositoryTest.kt
+++ data/src/test/java/com/danielwaiguru/weatherapp/data/repositories/WeatherRepositoryTest.kt
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Daniel Waiguru
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.danielwaiguru.weatherapp.data.repositories
 
 import app.cash.turbine.test
@@ -23,6 +47,7 @@ import org.junit.Test
 class WeatherRepositoryTest {
     @MockK
     val remoteDataSource = mockk<RemoteDataSource>()
+
     @MockK
     val localDataSource = mockk<LocalDataSource>()
     private lateinit var weatherRepository: WeatherRepository
@@ -57,8 +82,8 @@ class WeatherRepositoryTest {
                     awaitComplete()
                 }
         }
-
     }
+
     @Test
     fun `test weather forecast returns correctly data when network request fails`() {
         runTest {
@@ -80,7 +105,6 @@ class WeatherRepositoryTest {
                     awaitComplete()
                 }
         }
-
     }
 
     @Test
@@ -104,7 +128,6 @@ class WeatherRepositoryTest {
                     awaitComplete()
                 }
         }
-
     }
 
     @Test
@@ -128,7 +151,6 @@ class WeatherRepositoryTest {
                     awaitComplete()
                 }
         }
-
     }
 
     @Test
@@ -146,13 +168,12 @@ class WeatherRepositoryTest {
             weatherRepository.getCurrentWeather(testLocation)
                 .test {
                     awaitItem().also { result ->
-                        //Truth.assertThat(result).isInstanceOf(ResultWrapper.Success::class.java)
+                        // Truth.assertThat(result).isInstanceOf(ResultWrapper.Success::class.java)
                         Truth.assertThat(result.data).isNotNull()
                     }
                     awaitComplete()
                 }
         }
-
     }
 
     @Test
@@ -175,6 +196,5 @@ class WeatherRepositoryTest {
                     awaitComplete()
                 }
         }
-
     }
 }
